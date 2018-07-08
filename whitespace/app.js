77c816f12c0fce6e7fee94e8b8e274dc6a8c8b24
Jordi <jordifelipzayas@gmail.com>
2018-07-08 22:35:22 +0200

    whitespaces

---
+++ whitespace/app.js
@@ -0,0 +1,15 @@
+var 
+    // first name of the person
+    firstname, 
+    // last name of the person
+    lastname, 
+    // the language
+    // can be 'en or 'es'
+    language;
+
+var person = {
+    firstname : 'Jordi',
+    lastname : 'Felip'
+}
+
+console.log(person);
